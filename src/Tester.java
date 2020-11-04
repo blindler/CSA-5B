97c663e514139f0ca3ee477d0814746b6b2e2889
Barry Lindler <blindler@lexrich5.org>
2020-11-04 09:28:25 -0500

    added sample in tester.java

--- src/Tester.java
+++ src/Tester.java
@@ -3,6 +3,18 @@ public class Tester {
     public static void main (String[] args){
 
 
+                String s1 = new String("hat");
+                String s2 = new String("hat");
+                String s3 = s2;
+                boolean b1 = (s1 == s2);
+                boolean b2 = (s2 == s3);
+                boolean b3 = s1.equals(s2);
+                boolean b4 = "hat".equals(s1);
+                System.out.println(b1+" "+b2+" "+b3+" "+b4);
+
+
+
+
     }
 
 
