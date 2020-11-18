23e992939f5897162ec063857c2747940f2ef763
Barry Lindler <blindler@lexrich5.org>
2020-11-18 08:57:19 -0500

    added sample in tester 11.18.20

--- src/Tester.java
+++ src/Tester.java
@@ -2,20 +2,39 @@ public class Tester {
 
     public static void main (String[] args) {
 
-                String message = "Ix lovex youxxx";
-                System.out.println(message);
+        String message = "was it a cat I saw";
+        System.out.println(message);
+        String result = "";
+        // reverse it
+
+        for (int i = 0; i < message.length(); i++){
+            result = message.substring(i,i+1) + result;
+
+            System.out.println(result);
+        }
 
 
-                while ( message.indexOf("x") != -1 )
-                {
-                    int x = message.indexOf("x");
-                    String part1 = message.substring(0,x);
-                    String part2 = message.substring(x+1);
+        System.out.println(result);
 
-                    message = part1 + part2;
 
-                    System.out.println(message);
-                }
+
+
+
+
+
+
+
+
+//                while ( message.indexOf("x") != -1 )
+//                {
+//                    int x = message.indexOf("x");
+//                    String part1 = message.substring(0,x);
+//                    String part2 = message.substring(x+1);
+//
+//                    message = part1 + part2;
+//
+//                    System.out.println(message);
+//                }
 
 
 
