0f007e5c5cd381aca3faffc6a66be74a5f7e5d8f
blindler <blindler@lexrich5.org>
2020-11-20 11:29:08 -0500

    11/20/20

--- src/Tester.java
+++ src/Tester.java
@@ -3,10 +3,8 @@ public class Tester {
     public static void main(String[] args) {
 
     int i = 0;
-        do {
-            System.out.println("hey");
-            i++;
-        }while ( i < 10 );
+        for ( i = 0; i < 10; i++){}
+        System.out.println(i);
 
 
 
