2ba8bcd73f52be5d0398a4bbcfb52c91646d0510
blindler <blindler@lexrich5.org>
2020-12-11 12:06:00 -0500

    started adding StepTracker from Unit 005.13 Runestone

---
+++ src/StepTracker.java
@@ -0,0 +1,17 @@
+public class StepTracker
+{
+    // copy the instance variable declarations here
+
+
+    // copy the constructor with a parameter here
+
+    // Write the accessor method activeDays() here
+    // @return activeDays
+
+    public static void main(String[] args)
+    {
+//        StepTracker tr = new StepTracker(10000);
+//        System.out.println(tr.activeDays()); // returns 0. No data have been recorded yet.
+    }
+}
+
